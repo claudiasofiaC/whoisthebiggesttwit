fa10359ba86e269ba5027f192c91903f3c797c6a
claudiasofiaC <claudiasofiaC@users.noreply.github.com>
2019-12-18 09:53:14 -0500

    day 1 stuff

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -1,32 +1,19 @@
-"""Code for our app"""
+"""code for app"""
 
-from decouple import config
-from flask import Flask, render_template, request
-from .models import DB, User
-
-#make our app factory
+from flask import Flask
+from .models import DB
+# app factory
 
 def create_app():
     app = Flask(__name__)
 
-    #add config for database
-    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
-
-    #stop tracking modifications on sqlalchemy config
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-    #have the database know about the app
+# add config for db
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+# have the db know about the app
     DB.init_app(app)
+
 
     @app.route('/')
     def root():
-        users = User.query.all()
-        return render_template('base.html', title = 'Home', users=users)
-
-    @app.route('/reset')
-    def reset():
-        DB.drop_all()
-        DB.create_all()
-        return render_template('base.html', title = 'Reset', users=[])
-
+        return 'Welcome to Twitoff!'
     return app
--- TWITOFF/models.py
+++ TWITOFF/models.py
@@ -3,12 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 DB = SQLAlchemy()
 
 class User(DB.Model):
-"""Twitter user we analyze"""
-    id = DB.Column(DB.BigInterger, primary_key=True)
+#Twitter user we analyze
+    id = DB.Column(DB.BigInteger, primary_key=True)
     name = DB.Column(DB.String(15), nullable=False)
 
 
 class Tweet(DB.Model):
-    """tweets we pull"""
-    id = DB.Column(DB.BigInterger, primary_key=True)
+    #tweets we pull
+    id = DB.Column(DB.BigInteger, primary_key=True)
     text = DB.Column(DB.Unicode(280))
