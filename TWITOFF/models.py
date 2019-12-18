36e08710bbba1fc9bdcf70ab7f6d3ec883cf4bbd
claudiasofiaC <claudiasofiaC@users.noreply.github.com>
2019-12-18 10:29:02 -0500

    changing schema

--- TWITOFF/models.py
+++ TWITOFF/models.py
@@ -6,15 +6,18 @@ class User(DB.Model):
 #Twitter user we analyze
     id = DB.Column(DB.BigInteger, primary_key=True)
     name = DB.Column(DB.String(15), nullable=False)
-
+    newest_tweet_id = DB.Column(BigInteger)
+    def __repr__(self):
+        return '<User> {}>'.format(self.name)
 
 class Tweet(DB.Model):
     #tweets we pull
     id = DB.Column(DB.BigInteger, primary_key=True)
     text = DB.Column(DB.Unicode(300))
     user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
-    
-
+    user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
+    def __repr__(self):
+        return '<Tweet {}>'.format(self.text)
 
 
 
