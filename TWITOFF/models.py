92693a8053670527c3041f4f5f10b61dd6ec9ad2
claudiasofiaC <claudiasofiaC@users.noreply.github.com>
2019-12-18 08:58:49 -0500

    models

--- TWITOFF/models.py
+++ TWITOFF/models.py
@@ -2,15 +2,13 @@ from flask_sqlalchemy import SQLAlchemy
 
 DB = SQLAlchemy()
 
-# in DB use 2 tables to organize the classes
-# 1 class for users and 1 class for tweets
-class User(DB>Model):
-"""twitter users we analyze"""
+class User(DB.Model):
+"""Twitter user we analyze"""
     id = DB.Column(DB.BigInterger, primary_key=True)
     name = DB.Column(DB.String(15), nullable=False)
 
 
 class Tweet(DB.Model):
-"""tweets we pull"""
+    """tweets we pull"""
     id = DB.Column(DB.BigInterger, primary_key=True)
     text = DB.Column(DB.Unicode(280))
