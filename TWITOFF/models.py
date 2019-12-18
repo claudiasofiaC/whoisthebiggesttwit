2bbf3a77cff28de84e49c341a2ff15ca539c601d
claudiasofiaC <claudiasofiaC@users.noreply.github.com>
2019-12-18 10:18:29 -0500

    changing schema in models.py

--- TWITOFF/models.py
+++ TWITOFF/models.py
@@ -11,4 +11,12 @@ class User(DB.Model):
 class Tweet(DB.Model):
     #tweets we pull
     id = DB.Column(DB.BigInteger, primary_key=True)
-    text = DB.Column(DB.Unicode(280))
+    text = DB.Column(DB.Unicode(300))
+    user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
+    
+
+
+
+
+# what relationship do we want btwn User and Tweet?
+# one to many
